2f235c594cf5b5bcd643e62ef8ec4d9a5609b22d
vallespp <vallespp@gmail.com>
2018-04-05 21:53:38 -0300

    actualizanding para digitalOcean

--- nuevo.php
+++ nuevo.php
@@ -2,4 +2,56 @@
 	echo "Hola";
 	echo "<hr>";
 	echo "Mundo";
+	echo "<p>(para más información hacé click en el curso que más te interese)</p>
+            <button class="btn success">QGIS nivel I</button>
+            <button class="btn info">QGIS nivel II</button><br><br>
+
+            <p>Hola Heroku</p>
+        </center>
+
+        <style type="text/css">
+            body{
+              margin: 0px;
+            }
+
+            .btn {
+              border: 2px solid black;
+              border-radius: 5px;
+              background-color: white;
+              color: black;
+              padding: 14px 28px;
+              font-size: 22px;
+              cursor: pointer;
+              font-family: Century Gothic;
+              margin-bottom: 4px;
+            }
+
+            .success {
+              border-color: #FDDA01;
+              color: #FDDA01;
+            }
+
+            .success:hover {
+              background-color: #FDDA01;
+              color: white;
+            }
+
+            .info {
+              border-color: #FDDA01;
+              color: #FDDA01;
+            }
+
+            .info:hover {
+              background: #FDDA01;
+              color: white;
+            }
+
+            p{
+              font-family: Century Gothic;
+            }
+        </style>
+
+    </body>
+
+</html>"
 ?>
